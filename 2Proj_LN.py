75ee979d2c09bab8ca0a8f93930841819cefa966
SofiaAparicio <sofia.aparicio@tecnico.ulisboa.pt>
2017-11-05 19:17:49 +0000

    Bigrams done

--- 2Proj_LN.py
+++ 2Proj_LN.py
@@ -3,30 +3,61 @@
 ################################################
 import nltk
 from nltk import tokenize
+from nltk.util import ngrams, bigrams
 import codecs
-
+import string
 
 ################################################
 #                 functions                    #
 ################################################
 def run():
-    with codecs.open("virVerVir-1.final", "r", "latin-1") as file:
+    with codecs.open("virVerVir-1.final", "r", encoding='utf-8') as file:
         raw = (file.read())#.split('\n')#.decode('utf-8')
     file.close()
 
     tokens = tokenize.word_tokenize(raw, language = "portuguese")
     #see unigrams
-    tokens = [token.lower() for token in tokens]
+    punctuations = list(string.punctuation) #+ list(string.digits)
 
+    tokens = [token.lower() for token in tokens if token not in punctuations ] #if token not in punctuations
     unigrams = [(item, tokens.count(item)) for item in sorted(set(tokens))]
-    for un in unigrams:
-        print (un[0]).encode('utf-8')
-    #Create your bigrams
-    bgs = nltk.bigrams(tokens)
 
-    #compute frequency distribution for all the bigrams in the text
-    fdist = nltk.FreqDist(bgs)
-    # print("Frequency distribution for bigrams")
-    # for k,v in fdist.items():
-    #     print(k,v)
-run()
+    f = open('unigramasVer.txt', 'w')
+    for un in unigrams:
+        f.write( (un[0]).encode('UTF-8') + "\t" + str(un[1]) + "\n" ) #+ "\t" + str(un[1])
+    f.close()
+    print len(unigrams)
+
+
+    #see bigrams
+    sent = tokenize.sent_tokenize(raw, language = "portuguese")
+    new_bigrams = []
+    for line in sent:
+        tokens = tokenize.word_tokenize(line, language = "portuguese")
+        tokenLine = [token.lower() for token in tokens if token not in punctuations ] #if token not in punctuations
+
+        # bgs = nltk.bigrams(tokenLine)
+        new_bigrams += list(bigrams(tokenLine))
+
+
+    ordBigrams = [(item, new_bigrams.count(item)) for item in sorted(set(new_bigrams))]
+
+    f = open('bigramasVer.txt', 'w')
+    for un in ordBigrams:
+        f.write( (un[0][0]).encode('UTF-8') + " " + (un[0][1]).encode('UTF-8') + "\t" + str(un[1]) + "\n" ) #+ "\t" + str(un[1])
+    f.close()
+
+
+    # #bigrams mal
+    # tokens = tokenize.word_tokenize(raw, language = "portuguese")
+    #
+    # bi = list(set(bigrams(tokens)))
+    # print bi
+    # print len(bi)
+
+################################################
+#                     run                      #
+################################################
+
+if __name__ == '__main__':
+    run()
